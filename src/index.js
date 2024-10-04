e96e1c58144cd26441029c047ba46e4d74ed504f
mary913 <Patrivimwenge@gmail.com>
2024-10-04 15:08:33 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const taskForm = document.querySelector("#create-task-form");
+  const taskList = document.querySelector("#tasks");
+
+  taskForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const newTaskDescription = document.querySelector("#new-task-description").value;
+
+    if (newTaskDescription) {
+      const taskItem = document.createElement("li");
+      taskItem.textContent = newTaskDescription;
+      taskList.appendChild(taskItem);
+
+      // Clear the input field after adding the task
+      document.querySelector("#new-task-description").value = "";
+    }
+  });
 });
